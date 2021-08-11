8cb354dc7ee76a05ea83767e41697c8e80739e6a
3Mydlo3 <mydlo333@interia.pl>
2021-08-11 17:42:49 +0200

    Update ServerStatus DTO for new Manager version

--- ArmaForces.ArmaServerManager.Discord/Features/Server/DTOs/ServerStatus.cs
+++ ArmaForces.ArmaServerManager.Discord/Features/Server/DTOs/ServerStatus.cs
@@ -1,23 +1,23 @@
-﻿namespace ArmaForces.ArmaServerManager.Discord.Features.Server.DTOs
+﻿#nullable enable
+
+namespace ArmaForces.ArmaServerManager.Discord.Features.Server.DTOs
 {
     public class ServerStatus
     {
-        public int? HeadlessClientsConnected { get; set; }
+        public ServerStatusEnum Status { get; set; }
 
-        public bool IsServerStarting { get; set; }
+        public string? Name { get; set; }
         
-        public bool IsServerRunning { get; set; }
+        public string? ModsetName { get; set; }
 
-        public string ModsetName { get; set; }
+        public string? Map { get; set; }
 
-        public string Name { get; set; }
+        public int? Players { get; set; }
 
-        public string Map { get; set; }
+        public int? PlayersMax { get; set; }
 
-        public int Players { get; set; }
-
-        public int PlayersMax { get; set; }
-
-        public int Port { get; set; }
+        public int? Port { get; set; }
+        
+        public int? HeadlessClientsConnected { get; set; }
     }
 }
--- ArmaForces.ArmaServerManager.Discord/Features/Server/Helpers/ServerStatusEmbedHelpers.cs
+++ ArmaForces.ArmaServerManager.Discord/Features/Server/Helpers/ServerStatusEmbedHelpers.cs
@@ -1,92 +1,97 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using ArmaForces.ArmaServerManager.Discord.Features.Server.DTOs;
 using Discord;
 
 namespace ArmaForces.ArmaServerManager.Discord.Features.Server.Helpers
 {
-    internal class ServerStatusEmbedHelpers
+    internal static class ServerStatusEmbedHelpers
     {
         public static Embed CreateServerStatusEmbed(ServerStatus serverStatus)
         {
-            if (!serverStatus.IsServerRunning && !serverStatus.IsServerStarting)
+            return serverStatus.Status switch
             {
-                return new EmbedBuilder
-                {
-                    Title = "Server Status",
-                    Fields = new List<EmbedFieldBuilder>
-                    {
-                        new EmbedFieldBuilder
-                        {
-                            IsInline = false,
-                            Name = $"server:{serverStatus.Port}",
-                            Value = ":x: Server unavailable"
-                        }
-                    }
-                }.Build();
-            }
-
-            if (serverStatus.IsServerStarting)
-            {
-                return new EmbedBuilder
-                {
-                    Title = "Server Status",
-                    Fields = new List<EmbedFieldBuilder>
-                    {
-                        new EmbedFieldBuilder
-                        {
-                            IsInline = false,
-                            Name = $"server:{serverStatus.Port}",
-                            Value = ":arrows_counterclockwise: Server starting"
-                        },
-                        new EmbedFieldBuilder
-                        {
-                            IsInline = false,
-                            Name = "Modset name",
-                            Value = string.IsNullOrWhiteSpace(serverStatus.ModsetName) ? "No modset" : serverStatus.ModsetName
-                        }
-                    }
-                }.Build();
-            }
-
-            var embedBuilder = new EmbedBuilder
-            {
-                Title = "Server Status",
-                Fields = new List<EmbedFieldBuilder>
-                {
-                    new EmbedFieldBuilder
-                    {
-                        IsInline = false,
-                        Name = $"server:{serverStatus.Port}",
-                        Value = $@":white_check_mark: Server ""{serverStatus.Name}"" online"
-                    },
-                    new EmbedFieldBuilder
-                    {
-                        IsInline = false,
-                        Name = "Players online",
-                        Value = $"{serverStatus.Players}/{serverStatus.PlayersMax}"
-                    },
-                    new EmbedFieldBuilder
-                    {
-                        IsInline = false,
-                        Name = "Modset name",
-                        Value = string.IsNullOrWhiteSpace(serverStatus.ModsetName) ? "No modset" : serverStatus.ModsetName
-                    },
-                    new EmbedFieldBuilder
-                    {
-                        IsInline = false,
-                        Name = "Current map",
-                        Value = string.IsNullOrWhiteSpace(serverStatus.Map) ? "No map" : serverStatus.Map
-                    },
-                    new EmbedFieldBuilder
-                    {
-                        IsInline = false,
-                        Name = "Connected Headless Clients",
-                        Value = serverStatus.HeadlessClientsConnected ?? 0
-                    }
-                }
+                ServerStatusEnum.Stopped => BuildEmbedForStopped(serverStatus),
+                ServerStatusEnum.Starting => BuildEmbedForStarting(serverStatus),
+                ServerStatusEnum.Started => BuildEmbedForStarted(serverStatus),
+                _ => throw new ArgumentOutOfRangeException()
             };
-
-            return embedBuilder.Build();
         }
+
+        private static Embed BuildEmbedForStopped(ServerStatus serverStatus) => new EmbedBuilder
+        {
+            Title = "Server Status",
+            Fields = new List<EmbedFieldBuilder>
+            {
+                new EmbedFieldBuilder
+                {
+                    IsInline = false,
+                    Name = $"server:{serverStatus.Port}",
+                    Value = ":x: Server unavailable"
+                }
+            }
+        }.Build();
+
+        private static Embed BuildEmbedForStarting(ServerStatus serverStatus) => new EmbedBuilder
+        {
+            Title = "Server Status",
+            Fields = new List<EmbedFieldBuilder>
+            {
+                new EmbedFieldBuilder
+                {
+                    IsInline = false,
+                    Name = $"server:{serverStatus.Port}",
+                    Value = ":arrows_counterclockwise: Server starting"
+                },
+                new EmbedFieldBuilder
+                {
+                    IsInline = false,
+                    Name = "Modset name",
+                    Value = string.IsNullOrWhiteSpace(serverStatus.ModsetName)
+                        ? "No modset"
+                        : serverStatus.ModsetName
+                }
+            }
+        }.Build();
+
+        private static Embed BuildEmbedForStarted(ServerStatus serverStatus) => new EmbedBuilder
+        {
+            Title = "Server Status",
+            Fields = new List<EmbedFieldBuilder>
+            {
+                new EmbedFieldBuilder
+                {
+                    IsInline = false,
+                    Name = $"server:{serverStatus.Port}",
+                    Value = $@":white_check_mark: Server ""{serverStatus.Name}"" online"
+                },
+                new EmbedFieldBuilder
+                {
+                    IsInline = false,
+                    Name = "Players online",
+                    Value = $"{serverStatus.Players}/{serverStatus.PlayersMax}"
+                },
+                new EmbedFieldBuilder
+                {
+                    IsInline = false,
+                    Name = "Modset name",
+                    Value = string.IsNullOrWhiteSpace(serverStatus.ModsetName)
+                        ? "No modset"
+                        : serverStatus.ModsetName
+                },
+                new EmbedFieldBuilder
+                {
+                    IsInline = false,
+                    Name = "Current map",
+                    Value = string.IsNullOrWhiteSpace(serverStatus.Map) ? "No map" : serverStatus.Map
+                },
+                new EmbedFieldBuilder
+                {
+                    IsInline = false,
+                    Name = "Connected Headless Clients",
+                    Value = serverStatus.HeadlessClientsConnected ?? 0
+                }
+            }
+        }.Build();
     }
 }
